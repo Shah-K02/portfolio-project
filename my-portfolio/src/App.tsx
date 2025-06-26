f21a2b6c28a5b33f81f692d80a04aeaa3468a31b
shahk <shahkar0215@gmail.com>
2025-06-27 00:29:20 +0100

    Refactor App component structure for improved readability

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Introduction from './components/Introduction';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import WorkWithMe from './components/WorkWithMe';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Introduction />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+      <WorkWithMe />
     </div>
   );
 }
