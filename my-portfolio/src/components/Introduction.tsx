20291fad98cfe01cdae53887bcf4309b6ad5ca86
shahk <shahkar0215@gmail.com>
2025-06-27 17:35:56 +0100

    Add react-feather dependency and enhance Introduction component with animated profile circle

--- my-portfolio/src/components/Introduction.tsx
+++ my-portfolio/src/components/Introduction.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import { GitHubIcon, LinkedInIcon } from './Icons';
 import './Introduction.css';
+import { motion } from "framer-motion";
+import { Code } from "react-feather";
 
 const Introduction: React.FC = () => {
   return (
     <section className="introduction">
       <div className="container">
         <div className="introduction-content">
+          {/* Animated Profile Circle with Your Picture */}
           <div className="profile-image">
-            <img
-              src='./profilepic.jpeg'
-              alt="Profile"
-              className="profile-pic"
-            />
+            <motion.div
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, delay: 0.2 }}
+              className="profile-circle-container"
+            >
+              <div className="profile-circle-outer">
+                <div className="profile-circle-gradient">
+                  <div className="profile-circle-white">
+                    <div className="profile-circle-inner">
+                      <img 
+                        src="./profilepic.jpeg" 
+                        alt="Your Name"
+                        className="profile-picture"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <motion.div
+                  className="profile-circle-decoration"
+                  animate={{ rotate: 360 }}
+                  transition={{ duration: 20, repeat: Infinity, ease: "linear" }}
+                >
+                  <Code className="profile-icon" />
+                </motion.div>
+              </div>
+            </motion.div>
           </div>
+
           <h1 className="main-title">
             Hi, I'm <span className="highlight">Your Name</span>
           </h1>
