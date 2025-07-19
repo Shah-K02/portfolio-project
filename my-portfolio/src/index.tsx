37130dc460cd78c0b4f11310d5c41af2456ef1ff
shahk <shahkar0215@gmail.com>
2025-07-19 21:17:12 +0100

    perf(styles): add performance optimization styles
    Add CSS optimizations including content-visibility and will-change properties to improve rendering performance and reduce layout shifts

--- my-portfolio/src/index.tsx
+++ my-portfolio/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/theme.css';
 import './styles/accessibility.css';
+import './styles/performance.css';
 import './index.css';
 import App from './App';
 
